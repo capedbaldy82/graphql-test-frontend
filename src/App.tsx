7862009b04618e1b98ce11d9a0b4afa14236a6a2
chango <kimtothechang@gmail.com>
2022-10-05 16:10:03 +0900

    test: localField & cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import Movie from './routes/Movie';
-import Movies from './routes/Movies';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Reset } from "styled-reset";
+import Movie from "./routes/Movie";
+import Movies from "./routes/Movies";
 
 function App() {
   return (
     <div className="App">
+      <Reset />
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Movies />} />
-          <Route path='/movies/:id' element={<Movie />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/movies/:id" element={<Movie />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,21 +1,23 @@
 import { gql, useQuery } from "@apollo/client";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ImageType } from "./Movie";
 
 const ALL_MOVIES = gql`
   query {
     allMovies {
       id
       title
-    }
-    allTweets {
-      id
-      text
-      author {
-        fullName
-      }
+      medium_cover_image
     }
   }
 `;
+
+interface MoiveType {
+  id: string;
+  title: string;
+  medium_cover_image: string;
+}
 
 const Movies = () => {
   const { data, loading, error } = useQuery(ALL_MOVIES);
@@ -29,17 +31,78 @@ const Movies = () => {
   }
 
   return (
-    <div>
-      <h1>Movies</h1>
-      <ul>
-        {data?.allMovies?.map((movie: any) => (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
+    <Container>
+      <Header>
+        <Title>Apollo Movies</Title>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      <MoviesGrid>
+        {data?.allMovies?.map((movie: MoiveType) => (
+          <PosterContainer key={movie.id}>
+            <Link to={`/movies/${movie.id}`}>
+              <PosterBg bg={movie.medium_cover_image} />
+            </Link>
+          </PosterContainer>
         ))}
-      </ul>
-    </div>
+      </MoviesGrid>
+    </Container>
   );
 };
 
 export default Movies;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const MoviesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2vw;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+const PosterContainer = styled.div`
+  height: 25vw;
+  border-radius: 7px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  background-color: transparent;
+`;
+
+const PosterBg = styled.div`
+  background-image: url(${(props: ImageType) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 7px;
+`;
